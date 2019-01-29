9575deb00adf7b3eb0ad82a4e66ad0a95a370b3e
Jason Phillips <jaosn.phillips@hpfm.com>
2019-01-29 14:19:14 -0600

    full run through of mutation

--- keytest.java
+++ keytest.java
@@ -8,26 +8,36 @@ public class keytest {
     ArrayList<Character> constructorArray = new ArrayList<Character>();
     int keylength = key.length();
     int y=0;
+    int passes = 0;
+    int currentsize = 0;
     for(char x : key.toCharArray()){
       charKey.add(x);
     }
     mutatedKey = charKey;
-
     System.out.println("Key entered = "+ charKey.toString());
-    while(mutatedKey.size() <= 255){
-      for(int x=0; x <= mutatedKey.size(); x++){
-        constructorArray.add(mutatedKey.get(x));
-        if(x<=keylength){
-          constructorArray.add(charKey.get(y));
+    System.out.println("mutated Key = "+mutatedKey.toString());
 
+    while(mutatedKey.size() <= 255){
+      currentsize = mutatedKey.size();
+      for(int x=0; x <= currentsize; x++){
+        constructorArray.add(mutatedKey.get(x));
+
+        if(x<keylength-1){
+          constructorArray.add(charKey.get(y));
         }else{
           y = 0;
           constructorArray.add(charKey.get(y));
         }
         y++;
+        passes ++;
         mutatedKey = constructorArray;
-        System.out.println("mutated Key = "+mutatedKey.toString());
+        System.out.print("\rMutating Key:"+ (1/(255/key.length()))*100+"% pass #"+passes+" Key size = "+mutatedKey.size());
       }
+
+    }
+    System.out.print("\nmutated Key = ");
+    for(char x : mutatedKey){
+      System.out.print(x);
     }
   }
 }
